5e0106056b9cf8adc8e98ad7606281bb3de8cf23
brunobisognidev <brunobisogni5@gmail.com>
2020-02-03 17:20:13 -0300

    zerado a aplicação

--- backend/src/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const routes = express.Router();
-
-routes.get("/", (req, res) => {
-  return res.json({ message: `Olá ${req.query.name}` });
-});
-
-module.exports = routes;
